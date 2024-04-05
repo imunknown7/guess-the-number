7064bed64d4e90ddc3e61a5dcd95411fe3504dcf
iamunknown7 <sanjiith7@gmail.com>
2024-04-05 20:40:51 +0530

    v1 - basic guessing game

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+import random
+number = random.randrange(1, 10)
+i = 0
+while i < 3:
+    guess = int(input("Guess > "))
+    if guess == number:
+        print("Correct!")
+        break
+    else:
+        i += 1
+        print(f"Incorrect... Try again! Tries : {i}/3")
+print("The Number was ", number)
+print(f"Tries : {i}/3 ")
